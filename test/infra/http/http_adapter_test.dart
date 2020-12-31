c07dc932262975cf3a4f6a0c223b21c7bf357027
Renan Almeida <renan_lopes_almeida@hotmail.com>
2020-12-31 13:32:36 -0300

    ensure httpAdapter calls post without Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -23,7 +23,9 @@ class HttpAdapter {
       'accept': 'application/json',
     };
 
-    await client.post(url, headers: headers, body: jsonEncode(body));
+    final bodyRequest = (body != null) ? jsonEncode(body) : null;
+
+    await client.post(url, headers: headers, body: bodyRequest);
   }
 }
 
@@ -51,6 +53,17 @@ main() {
             'accept': 'application/json',
           },
           body: '{"any_key":"any_value"}',
+        ),
+      );
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(
+        client.post(
+          any,
+          headers: anyNamed('headers'),
         ),
       );
     });
