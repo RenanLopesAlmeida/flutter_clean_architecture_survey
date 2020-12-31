c0280404b7bda7c2c53c241d5df12e358f8fbfc2
Renan Almeida <renan_lopes_almeida@hotmail.com>
2020-12-31 14:41:39 -0300

    ensure HttpAdapter returns null if post returns 204

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -91,8 +91,16 @@ main() {
       expect(response, {'any_key': 'any_value'});
     });
 
-    test('Should return data if post returns 200 with no data', () async {
+    test('Should return null if post returns 200 with no data', () async {
       mockResponse(200, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204', () async {
+      mockResponse(204, body: '');
 
       final response = await sut.request(url: url, method: 'post');
 
