617c07f522064298f6db1b89bda7fd3776f21c75
Renan Almeida <renan_lopes_almeida@hotmail.com>
2021-01-02 09:32:31 -0300

    ensure HttpAdapter throws unauthorized on status code 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -105,6 +105,14 @@ main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
+    test('Should return BadRequestError if post returns 401', () async {
+      mockResponse(401);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
 
