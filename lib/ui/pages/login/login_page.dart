56f2ba43503905ba8543c2e115a03d7ade86aae8
Renan Almeida <renan_lopes_almeida@hotmail.com>
2021-01-20 19:58:41 -0300

    ensure LoginPage presents loading

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -3,82 +3,121 @@ import 'package:flutter/material.dart';
 import '../../components/components.dart';
 import 'login_presenter.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   final LoginPresenter presenter;
   LoginPage(this.presenter);
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    widget.presenter.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          LoginHeader(),
-          Headline1(label: 'LOGIN'),
-          Padding(
-            padding: const EdgeInsets.all(32),
-            child: Form(
-              child: Column(
-                children: [
-                  StreamBuilder<String>(
-                      stream: presenter.emailErrorStream,
-                      builder: (_, snapshot) {
-                        return TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          onChanged: presenter.validateEmail,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            errorText: snapshot.data?.isEmpty == true
-                                ? null
-                                : snapshot.data,
-                            icon: Icon(
-                              Icons.email,
-                              color: Theme.of(context).primaryColorLight,
-                            ),
-                          ),
-                        );
-                      }),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0, bottom: 32),
-                    child: StreamBuilder<String>(
-                        stream: presenter.passwordErrorStream,
-                        builder: (_, snapshot) {
-                          return TextFormField(
-                            obscureText: true,
-                            onChanged: presenter.validatePassword,
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              errorText: snapshot.data?.isEmpty == true
-                                  ? null
-                                  : snapshot.data,
-                              icon: Icon(
-                                Icons.lock,
-                                color: Theme.of(context).primaryColorLight,
-                              ),
-                            ),
-                          );
-                        }),
+      body: Builder(
+        builder: (context) {
+          widget.presenter.isLoadingStream.listen((isLoading) {
+            if (isLoading) {
+              showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (context) => SimpleDialog(
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircularProgressIndicator(),
+                        SizedBox(height: 10),
+                        Text('Loading...', textAlign: TextAlign.center),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }
+          });
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                LoginHeader(),
+                Headline1(label: 'LOGIN'),
+                Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Form(
+                    child: Column(
+                      children: [
+                        StreamBuilder<String>(
+                            stream: widget.presenter.emailErrorStream,
+                            builder: (_, snapshot) {
+                              return TextFormField(
+                                keyboardType: TextInputType.emailAddress,
+                                onChanged: widget.presenter.validateEmail,
+                                decoration: InputDecoration(
+                                  labelText: 'Email',
+                                  errorText: snapshot.data?.isEmpty == true
+                                      ? null
+                                      : snapshot.data,
+                                  icon: Icon(
+                                    Icons.email,
+                                    color: Theme.of(context).primaryColorLight,
+                                  ),
+                                ),
+                              );
+                            }),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0, bottom: 32),
+                          child: StreamBuilder<String>(
+                              stream: widget.presenter.passwordErrorStream,
+                              builder: (_, snapshot) {
+                                return TextFormField(
+                                  obscureText: true,
+                                  onChanged: widget.presenter.validatePassword,
+                                  decoration: InputDecoration(
+                                    labelText: 'Password',
+                                    errorText: snapshot.data?.isEmpty == true
+                                        ? null
+                                        : snapshot.data,
+                                    icon: Icon(
+                                      Icons.lock,
+                                      color:
+                                          Theme.of(context).primaryColorLight,
+                                    ),
+                                  ),
+                                );
+                              }),
+                        ),
+                        StreamBuilder<bool>(
+                            stream: widget.presenter.isFormValidStream,
+                            builder: (context, snapshot) {
+                              return RaisedButton(
+                                onPressed: snapshot.data == true
+                                    ? widget.presenter.auth
+                                    : null,
+                                child: Text('Login'.toUpperCase()),
+                              );
+                            }),
+                        FlatButton.icon(
+                            onPressed: () {},
+                            icon: Icon(Icons.person),
+                            label: Text('Create Account'))
+                      ],
+                    ),
                   ),
-                  StreamBuilder<bool>(
-                      stream: presenter.isFormValidStream,
-                      builder: (context, snapshot) {
-                        return RaisedButton(
-                          onPressed:
-                              snapshot.data == true ? presenter.auth : null,
-                          child: Text('Login'.toUpperCase()),
-                        );
-                      }),
-                  FlatButton.icon(
-                      onPressed: () {},
-                      icon: Icon(Icons.person),
-                      label: Text('Create Account'))
-                ],
-              ),
+                ),
+              ],
             ),
-          ),
-        ],
+          );
+        },
       ),
-    ));
+    );
   }
 }
--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -1,9 +1,11 @@
 abstract class LoginPresenter {
   Stream get emailErrorStream;
   Stream get passwordErrorStream;
-  Stream get isFormValidStream;
+  Stream<bool> get isFormValidStream;
+  Stream<bool> get isLoadingStream;
 
   void validateEmail(String email);
   void validatePassword(String password);
   void auth();
+  void dispose();
 }
