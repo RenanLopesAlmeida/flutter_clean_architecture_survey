cba880d57de70ba909e1ffbabd81856186a86e0f
Renan Almeida <renan_lopes_almeida@hotmail.com>
2021-04-28 19:26:35 -0300

    ensure EmailValidation returns null if email is valid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -22,4 +22,8 @@ void main() {
   test('Should return null if email is null', () {
     expect(sut.validate(null), null);
   });
+
+  test('Should return null if email is valid', () {
+    expect(sut.validate('user@gmail.com'), null);
+  });
 }
