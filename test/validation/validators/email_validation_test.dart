d71feb7d6e440f8a8475c3a1abfdf4ad345ceffc
Renan Almeida <renan_lopes_almeida@hotmail.com>
2021-04-28 19:45:25 -0300

    ensure EmailValidation returns an error message if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,11 +1,20 @@
 import 'package:flutter_clean_architecture_survey/validation/dependencies/dependencies.dart';
 import 'package:test/test.dart';
 
+const invalidEmailMessage = 'Invalid Field';
+
 class EmailValidation implements FieldValidation {
   const EmailValidation(this.field);
   final String field;
 
-  String validate(String value) => null;
+  String validate(String value) {
+    final regex = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+    );
+
+    final isValid = value?.isEmpty == true || regex.hasMatch(value);
+    return (isValid) ? null : invalidEmailMessage;
+  }
 }
 
 void main() {
@@ -25,5 +34,9 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate('user@gmail.com'), null);
+  });
+
+  test('Should return an error message if email is invalid', () {
+    expect(sut.validate('user@gmail'), invalidEmailMessage);
   });
 }
