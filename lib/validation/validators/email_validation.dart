18c9d998ce2a83fecaed158d9b2cea442521c6c9
Renan Almeida <renan_lopes_almeida@hotmail.com>
2021-04-28 19:56:24 -0300

    moved EmailValidation to its own file and fixing logic in case email is null

---
+++ lib/validation/validators/email_validation.dart
@@ -0,0 +1,16 @@
+import '../dependencies/dependencies.dart';
+
+class EmailValidation implements FieldValidation {
+  const EmailValidation(this.field);
+  final String field;
+
+  String validate(String value) {
+    final regex = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+    );
+
+    final isValid =
+        value == null || value?.isEmpty == true || regex.hasMatch(value);
+    return (isValid) ? null : 'Invalid Field';
+  }
+}
--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,21 +1,7 @@
-import 'package:flutter_clean_architecture_survey/validation/dependencies/dependencies.dart';
 import 'package:test/test.dart';
+import '../../../lib/validation/validators/validators.dart';
 
 const invalidEmailMessage = 'Invalid Field';
-
-class EmailValidation implements FieldValidation {
-  const EmailValidation(this.field);
-  final String field;
-
-  String validate(String value) {
-    final regex = RegExp(
-      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
-    );
-
-    final isValid = value?.isEmpty == true || regex.hasMatch(value);
-    return (isValid) ? null : invalidEmailMessage;
-  }
-}
 
 void main() {
   EmailValidation sut;
